3e2cc97f764addd6c70ab22879edb0ddf3543eea
mervcelik <mrv.ozturrk@gmail.com>
2025-08-30 21:54:39 +0300

    Blog kısmı için başlangıç yapıldı

---
+++ InsureYouAI/ViewComponenets/DefaultViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponenets.DefaultViewComponents;
+
+public class _DefaultHeadComponentPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ InsureYouAI/ViewComponenets/DefaultViewComponents/_DefaultHeaderComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponenets.DefaultViewComponents;
+
+public class _DefaultHeaderComponentPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
